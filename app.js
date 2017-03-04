4ed223b8dbea3526aea1ca02aabf9129721b2e2d
Galillei <bazilio.rodger@gmail.com>
2017-03-04 14:31:23 +0300

    Add telegram bot

--- app.js
+++ app.js
@@ -1,51 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use strict";
 var mongoose = require('mongoose');
-var index = require('./routes/index');
-var users = require('./routes/users');
 mongoose.connect('mongodb://db/telebot-money');
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('Connected!');
 });
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const TeleBot = require('telebot');
+const bot = new TeleBot('374313292:AAH8HU2esxr1fF0oJl6bsQSJudUkPYk3zK8');
+bot.on('text', msg => {
+    let fromId = msg.from.id;
+let firstName = msg.from.first_name;
+let reply = msg.message_id;
+return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+bot.connect();
