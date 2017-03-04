7189bf21a1a13cbaa7ebe118ac7f5073c75a257c
Galillei <bazilio.rodger@gmail.com>
2017-03-04 14:43:39 +0300

    Add config file

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 "use strict";
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://db/telebot-money');
+var config = require('config/config');
+mongoose.connect(config.mongoUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('Connected!');
 });
 const TeleBot = require('telebot');
-const bot = new TeleBot('374313292:AAH8HU2esxr1fF0oJl6bsQSJudUkPYk3zK8');
+const bot = new TeleBot(config.secretKeyTelegramm);
 bot.on('text', msg => {
     let fromId = msg.from.id;
 let firstName = msg.from.first_name;
